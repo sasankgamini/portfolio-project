880bb17c305d5818eb8391cc0f7b023e6ea04068
16BitNarwhal <ericzh1616@gmail.com>
2024-06-20 19:21:54 -0400

    setup routes

--- app/__init__.py
+++ app/__init__.py
@@ -9,3 +9,15 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"))
+
+@app.route('/hobbies')
+def hobbies():
+    return render_template('index.html', title="Hobbies", url=os.getenv("URL"))
+
+@app.route('/work_experiences')
+def work_experiences():
+    return render_template('index.html', title="Work Experiences", url=os.getenv("URL"))
+
+@app.route('/education')
+def education():
+    return render_template('index.html', title="Education", url=os.getenv("URL"))
