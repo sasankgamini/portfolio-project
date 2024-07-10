f4e04ac18f4f4f9caf69a39e52d3db876ae46195
sasankgamini <sasankgamini@gmail.com>
2024-07-09 23:48:54 -0700

    created save and retreival endpoints for TimelinePost database

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,13 @@ import os
 from flask import Flask, render_template, request
 from dotenv import load_dotenv
 from peewee import *
+import datetime
+from playhouse.shortcuts import model_to_dict
 
 load_dotenv()
 app = Flask(__name__)
 
+#connecting to MySQL server with environmental variables
 mydb = MySQLDatabase(
     os.getenv("MYSQL_DATABASE"),
     user = os.getenv("MYSQL_USER"),
@@ -15,6 +18,20 @@ mydb = MySQLDatabase(
 )
 
 print(mydb)
+
+#creating sql table
+class TimelinePost(Model):
+    name = CharField()
+    email = CharField()
+    content = TextField()
+    created_at = DateTimeField(default=datetime.datetime.now)
+
+    class Meta:
+        database = mydb
+
+mydb.connect()
+mydb.create_tables([TimelinePost])
+
 
 name = "Sasank Gamini"
 
@@ -87,3 +104,23 @@ def education():
         }
     ]
     return render_template('education.html', name=name, title="Education", educations = educations, url=os.getenv("URL"))
+
+#save endpoint for TimelinePost ORM model
+@app.route('/api/timeline_post', methods = ["POST"])
+def post_time_line_post():
+    name = request.form['name']
+    email = request.form['email']
+    content = request.form['content']
+    timeline_post = TimelinePost.create(name=name, email=email, content=content)
+
+    return model_to_dict(timeline_post)
+
+#retreive endpoint for TimelinePost ORM model
+@app.route('/api/timeline_post', methods=["GET"])
+def get_time_line_post():
+    return {
+        'timeline_posts' : [
+            model_to_dict(p)
+            for p in TimelinePost.select().order_by(TimelinePost.created_at.desc())
+        ]
+    }
