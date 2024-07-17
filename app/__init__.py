7b3469646748e49c25a37e1b6022f442d6f8c2a7
16BitNarwhal <ericzh1616@gmail.com>
2024-07-17 04:05:02 +0000

    test db

--- app/__init__.py
+++ app/__init__.py
@@ -9,13 +9,15 @@ load_dotenv()
 app = Flask(__name__)
 
 #connecting to MySQL server with environmental variables
-mydb = MySQLDatabase(
-    os.getenv("MYSQL_DATABASE"),
-    user = os.getenv("MYSQL_USER"),
-    password = os.getenv("MYSQL_PASSWORD"),
-    host = os.getenv("MYSQL_HOST"),
-    port = 3306
-)
+if os.getenv("TESTING") == "true":
+    print("Running in test mode")
+    mydb = SqliteDatabase('file:memory?mode=memory&cache=shared', uri=True)
+else:    
+    mydb = MySQLDatabase(os.getenv("MYSQL_DATABASE"),
+              user=os.getenv("MYSQL_USER"),
+              password=os.getenv("MYSQL_PASSWORD"),
+              host=os.getenv("MYSQL_HOST"),
+              port=3306)
 
 print(mydb)
 
