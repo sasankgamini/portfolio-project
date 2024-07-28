ea1465a6c0089572b475be2719696eedd9651267
sasankgamini <sasankgamini@gmail.com>
2024-07-28 20:50:51 +0530

    update redeploy script for vps with docker implementation

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -6,10 +6,6 @@ cd /root/portfolio-project
 #updating project folder to have latest changes from the main branch on GitHub.
 git fetch && git reset origin/main --hard
 
-#entering python virtual environment and installing required dependencies
-source python3-virtualenv/bin/activate
-pip install -r requirements.txt
+docker compose -f docker-compose.prod.yml down
 
-#restarting the myportfolio systemd service, so that the changes above get applied
-systemctl daemon-reload
-systemctl restart myportfolio
+docker compose -f docker-compose.prod.yml up -d --build
